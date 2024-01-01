fecef4d501e25209d0997279ea252f3f3a0ba8e8
M.Suleimanov <M.Suleimanov@tanto-s.ru>
2024-01-01 21:37:06 +0300

    git init

---
+++ src/test/java/ru/suleimanov/scrapper/ScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.suleimanov.scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
